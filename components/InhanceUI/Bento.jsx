e78c065240c4f156fb948b79a846b4a8bd73553c
Chaynes-MacBookPro <ChayneW-MacBookPro.local>
2024-11-10 14:53:06 -0500

    testing opacity and text on mobile for projects sections.

--- components/InhanceUI/Bento.jsx
+++ components/InhanceUI/Bento.jsx
@@ -178,8 +178,7 @@ const AboutBlock = () => (
       My passion is building cool stuff.{" "}
       <span className="text-zinc-400">
         I build primarily with React, Tailwind CSS, and Framer Motion. I love
-        this stack so much that I even built a website about it. I've made over
-        a hundred videos on the subject across YouTube and TikTok.
+        this stack so much that I even built a website about it.
       </span>
     </p>
   </Block>
