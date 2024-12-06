be322c6001ffa888e453737a5fba018c750f3676
Chaynes-MacBookPro <ChayneW-MacBookPro.local>
2024-12-05 20:05:29 -0500

    removed extra component call.

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import SplashScreen from "@/components/SplashScreen";
